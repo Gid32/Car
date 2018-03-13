54bb2d0eba0d8d1e36018d6c3b09cab4e99c3c39
Promsky Ivan <ivn_prmsk@ukr.net>
2018-03-14 00:07:07 +0200

    upgrade

--- src/ua/epamtask/Parameters.java
+++ src/ua/epamtask/Parameters.java
@@ -13,7 +13,7 @@ public class Parameters {
 	static{
 		models = new HashMap<String, String[]>();
 		models.put("Audi", new String[]{"A4","A5","A6"});
-		models.put("Audi", new String[]{"A4","A5","A6"});
+		models.put("Mercedes-Benz", new String[]{"SL","SM","SS"});
 	}
 	
 	static public int getRandomyearOfProduction(){
@@ -21,41 +21,39 @@ public class Parameters {
 	}
 
 	static public String getRandomMark(){
-		String Mark = models.keySet().toArray(new String[0])[(int) ((models.keySet().size()-1)*Math.random())];
+		String Mark = models.keySet().toArray(new String[0])[(int) ((models.keySet().size())*Math.random())];
 		
 		return Mark;
 	}
 	
 	static public String getRandomModel(String mark){
-		String model = models.get(mark)[(int) ((models.get(mark).length-1)*Math.random())];
+		String model = models.get(mark)[(int) ((models.get(mark).length)*Math.random())];
 		return model;
 	}
 	
 	static public String getRandomColor(){
-		int i = (int) (Math.random()*12);
+		int i = (int) (Math.random()*11);
 		switch (i){
 			 case 1: return "BLUE";
 			 case 2: return "CYAN";
 			 case 3: return "GRAY";
-			 case 4: return "GRAY";
-			 case 5: return "GREEN";
-			 case 6: return "GRAY";
-			 case 7: return "MAGENTA";
-			 case 8: return "ORANGE";
-			 case 9: return "PINK";
-			 case 10: return "RED";
-			 case 11: return "WHITE";
-			 case 12: return "YELLOW";
+			 case 4: return "GREEN";
+			 case 5: return "MAGENTA";
+			 case 6: return "ORANGE";
+			 case 7: return "PINK";
+			 case 8: return "RED";
+			 case 9: return "WHITE";
+			 case 10: return "YELLOW";
 			 default: return "BLACK";
 		}
 	}
 	
 	static public int getRandomPrice(){
-		return (int) (Math.random()*1000000)+100000;
+		return (int) (Math.random()*1000000)+1000;
 	}
 	
 	static public String getRandomRegistrationNumber(){
-		 return Double.toHexString(Math.random()*Long.MAX_VALUE);
+		 return Long.toHexString((long) (Math.random()*Long.MAX_VALUE));
 	}
 
 	public static int getCurrentYear() {

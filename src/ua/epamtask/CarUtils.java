ea01ff9250ed2df68a57e31364c10cb55e34e692
John <ivan.horpynych@gmail.com>
2018-03-15 19:39:58 +0200

    hotfix

--- src/ua/epamtask/CarUtils.java
+++ src/ua/epamtask/CarUtils.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class CarUtils {
 
     public static ArrayList<Car> getCarsOfGivenMark(String mark, ArrayList<Car> cars) {
-        ArrayList<Car> givenCars = new ArrayList<Car>();
+        ArrayList<Car> givenCars = new ArrayList<>();
         for (Car car : cars) {
             if (car.getMark().equals(mark)) givenCars.add(car);
         }
@@ -14,16 +14,16 @@ public class CarUtils {
     }
 
     public static ArrayList<Car> getCarsOfGivenModelAndLifeTime(String model, int lifeTime, ArrayList<Car> cars) {
-        ArrayList<Car> givenCars = new ArrayList<Car>();
+        ArrayList<Car> givenCars = new ArrayList<>();
         for (Car car : cars) {
-            if (car.getMark().equals(model) && (Parameters.getCurrentYear() - car.getYearOfProduction()) > lifeTime)
+            if (car.getModel().equals(model) && (Parameters.getCurrentYear() - car.getYearOfProduction()) > lifeTime)
                 givenCars.add(car);
         }
         return givenCars;
     }
 
     public static ArrayList<Car> getCarsOfGivenYearOfProductionAndPrice(int yearOfProduction, int price, ArrayList<Car> cars) {
-        ArrayList<Car> givenCars = new ArrayList<Car>();
+        ArrayList<Car> givenCars = new ArrayList<>();
         for (Car car : cars) {
             if (car.getYearOfProduction() == yearOfProduction && car.getPrice() > price) givenCars.add(car);
         }
--- src/ua/epamtask/Runner.java
+++ src/ua/epamtask/Runner.java
@@ -7,11 +7,13 @@ public class Runner {
     public static void main(String[] args) {
 
         ArrayList<Car> carMassive = new ArrayList<>();
-        for (int i = 0; i < 20; i++) carMassive.add(CarFactory.createCar());
+        for (int i = 0; i < 200; i++) carMassive.add(CarFactory.createCar());
 
         for (Car car : CarUtils.getCarsOfGivenMark("Audi", carMassive)) System.out.println(car.toString());
+        System.out.println();
 
         for (Car car : CarUtils.getCarsOfGivenModelAndLifeTime("A6", 5, carMassive)) System.out.println(car.toString());
+        System.out.println();
 
         for (Car car : CarUtils.getCarsOfGivenYearOfProductionAndPrice(1995, 1000, carMassive)) System.out.println(car.toString());
     }
